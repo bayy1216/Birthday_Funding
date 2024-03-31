a96ba6070b5e988a1c2160d102e9b8053c01d466
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-01 01:28:21 +0900

    #1 [Feat]: 기프티콘 컨트롤러 스웨거 추가

--- src/main/java/team/haedal/gifticionfunding/controller/gifticon/GifticonController.java
+++ src/main/java/team/haedal/gifticionfunding/controller/gifticon/GifticonController.java
@@ -1,5 +1,7 @@
 package team.haedal.gifticionfunding.controller.gifticon;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,37 +16,42 @@ import team.haedal.gifticionfunding.dto.gifticon.response.GifticonDetailDto;
 import team.haedal.gifticionfunding.dto.gifticon.response.GifticonDto;
 import team.haedal.gifticionfunding.service.gifticon.GifticonService;
 
+@Tag(name = "기프티콘", description = "기프티콘 관련 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GifticonController {
     private final GifticonService gifticonService;
 
+    @Operation(summary = "기프티콘 목록 조회", description = "기프티콘 목록 페이징 조회")
     @GetMapping("/api/gifticons")
     @ResponseStatus(HttpStatus.OK)
     public PagingResponse<GifticonDto> getGifticons(@RequestParam GifticonsQuery query) {
         return gifticonService.getGifticons(query.toPageRequest(), query.toSearch());
     }
 
+    @Operation(summary = "기프티콘 상세 조회", description = "기프티콘 상세 조회")
     @GetMapping("/api/gifticons/{gifticonId}")
     @ResponseStatus(HttpStatus.OK)
     public GifticonDetailDto getGifticon(@PathVariable Long gifticonId) {
         return gifticonService.getGifticon(gifticonId);
     }
 
-
+    @Operation(summary = "기프티콘 생성", description = "기프티콘 생성, 성공시 생성된 기프티콘의 id 반환")
     @PostMapping("/api/gifticons")
     @ResponseStatus(HttpStatus.CREATED)
     public Long createGifticon(@RequestBody @Valid GifticonCreateRequest request) {
         return gifticonService.createGifticon(request.toCommand());
     }
 
+    @Operation(summary = "기프티콘 수정", description = "기프티콘 수정")
     @PutMapping("/api/gifticons/{gifticonId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateGifticon(@PathVariable Long gifticonId, @RequestBody @Valid GifticonUpdateRequest request) {
         gifticonService.updateGifticon(gifticonId, request.toCommand());
     }
 
+    @Operation(summary = "기프티콘 삭제", description = "기프티콘 삭제")
     @DeleteMapping("/api/gifticons/{gifticonId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteGifticon(@PathVariable Long gifticonId) {
