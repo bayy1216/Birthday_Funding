6ac4cb81d3219dd7338e26c5389c9d97736bc00f
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-18 12:59:58 +0900

    #2 [Feat]: 친구요청 엔터티 추가

---
+++ src/main/java/team/haedal/gifticionfunding/domain/FriendShipActionStatus.java
@@ -0,0 +1,9 @@
+package team.haedal.gifticionfunding.domain;
+
+public enum FriendShipActionStatus {
+    REQUEST, // 요청
+    ACCEPT,  // 수락
+    REJECT, // 거절
+    CANCEL, // 취소
+    RECOMMEND // 추천
+}
--- src/main/java/team/haedal/gifticionfunding/entity/user/FriendshipAction.java
+++ src/main/java/team/haedal/gifticionfunding/entity/user/FriendshipAction.java
@@ -2,28 +2,38 @@ package team.haedal.gifticionfunding.entity.user;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import team.haedal.gifticionfunding.domain.FriendShipActionStatus;
 import team.haedal.gifticionfunding.entity.common.BaseTimeEntity;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Friendship extends BaseTimeEntity {
-
+public class FriendshipAction extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_1_id")
+    @JoinColumn(name = "from_user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private User user1;
+    private User fromUser;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_2_id")
+    @JoinColumn(name = "to_user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    private User user2;
+    private User toUser;
 
+    @Enumerated(EnumType.STRING)
+    private FriendShipActionStatus status;
+
+    @Builder
+    private FriendshipAction(User fromUser, User toUser, FriendShipActionStatus status) {
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.status = status;
+    }
 }
