fe4cdb34ed8afc173ab48fce371807630e96ccc1
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-04-28 21:30:31 +0900

    #3 [Chore]: TODO query-article

--- src/main/java/team/haedal/gifticionfunding/repository/funding/FundingQueryRepository.java
+++ src/main/java/team/haedal/gifticionfunding/repository/funding/FundingQueryRepository.java
@@ -4,17 +4,11 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import team.haedal.gifticionfunding.entity.funding.FundingArticle;
-import team.haedal.gifticionfunding.entity.funding.FundingArticleGifticon;
-import team.haedal.gifticionfunding.entity.funding.FundingContribute;
-import team.haedal.gifticionfunding.entity.funding.QFundingContribute;
 
 import java.util.List;
 
 import static team.haedal.gifticionfunding.entity.funding.QFundingArticle.fundingArticle;
 import static team.haedal.gifticionfunding.entity.funding.QFundingArticleGifticon.fundingArticleGifticon;
-import static team.haedal.gifticionfunding.entity.funding.QFundingContribute.fundingContribute;
-import static team.haedal.gifticionfunding.entity.gifticon.QGifticon.gifticon;
-import static team.haedal.gifticionfunding.entity.gifticon.QUserGifticon.userGifticon;
 
 @Repository
 @RequiredArgsConstructor
@@ -23,15 +17,14 @@ public class FundingQueryRepository {
 
     public FundingArticle findByIdWithUserOrThrow(Long fundingId) {
         FundingArticle article = queryFactory
-                .selectFrom(fundingArticle)
+                .select(fundingArticle)
+                .from(fundingArticle)
                 .join(fundingArticle.user).fetchJoin()
-                .join(fundingArticle.fundingArticleGifticons, fundingArticleGifticon).fetchJoin()
-                .join(fundingArticleGifticon.gifticon).fetchJoin()
-                .join(fundingArticle.fundingContributes, fundingContribute).fetchJoin()
-                .join(fundingContribute.userGifticon, userGifticon).fetchJoin()
-                .join(userGifticon.gifticon, gifticon).fetchJoin()
+                .leftJoin(fundingArticle.fundingArticleGifticons, fundingArticleGifticon).fetchJoin()
                 .where(fundingArticle.id.eq(fundingId))
                 .fetchOne();
+
+
         if(article == null) {
             throw new IllegalArgumentException("해당 펀딩이 존재하지 않습니다.");
         }
