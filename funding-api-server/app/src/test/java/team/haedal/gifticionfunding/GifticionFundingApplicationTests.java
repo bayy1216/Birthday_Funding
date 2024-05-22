7112a29c2d365253ab5793603e9633b7383a7fb1
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-05-22 21:53:36 +0900

    [스프링부트 자바 멀티모듈 적용]

--- funding-api-server/app/src/test/java/team/haedal/gifticionfunding/GifticionFundingApplicationTests.java
+++ funding-api-server/app/src/test/java/team/haedal/gifticionfunding/GifticionFundingApplicationTests.java
@@ -2,12 +2,14 @@ package team.haedal.gifticionfunding;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import test.TestClass;
 
 @SpringBootTest
 class GifticionFundingApplicationTests {
 
     @Test
     void contextLoads() {
+        TestClass testClass = new TestClass();
     }
 
 }
